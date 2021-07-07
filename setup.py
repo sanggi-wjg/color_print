97818e5b91eed2f9bc5f5ca7d9848b3a354d9842
우종구/v_A개발팀/GD <jonggu-woo@godo.co.kr>
2021-07-08 00:10:51 +0900

    Init

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'colorful-Print',
+    name = 'colorful-print',
     version = '0.0.1',
     url = 'https://github.com/sanggi-wjg/color_print.git',
     author = 'SangGi',
